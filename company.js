45e17f23abc379bd306c0cd954ecaf52737adcaa
AShSh <shshatova@gmail.com>
2017-10-03 17:02:27 +0300

    location block

--- company.js
+++ company.js
@@ -8,7 +8,7 @@ $(function () {
                 $('.global-alert').text('not connection');
                 $('.global-alert').show();
             } else {
-                callback(data);
+                callback(data.list);
             }
         },
         error: function () {
@@ -18,6 +18,32 @@ $(function () {
     });
 });
 
-function callback(data) {
-    console.log(data);
+function callback(list) {
+    totalCompany(list);
+
+    companyByLocation(list);
+}
+
+function totalCompany(list) {
+    $('#totalCompany .panel-body').html('<p class="company-all">' + (list.length - 1) + '</p>');
+}
+
+function companyByLocation(list) {
+    var countryCode = [];
+    var result = {};
+
+    list.forEach(function (item, i, arr) {
+        countryCode[i] = item.location.code;
+    });
+
+    for (var i = 0; i < countryCode.length; ++i) {
+        var a = countryCode[i];
+        if (result[a] != undefined) {
+            ++result[a];
+        } else {
+            result[a] = 1;
+        }
+    }
+
+
 }
